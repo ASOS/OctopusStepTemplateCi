20996fac7e15b0fc674d2708abee00e72426cc7d
Michael Clayton <Michael.Clayton@asos.com>
2017-10-05 13:41:26 +0100

    Added new Get-*FromScriptText functions

---
+++ OctopusStepTemplateCi/Cmdlets/Internal/PowerShellManipulation/Get-ScriptBodyFromScriptText.ps1
@@ -0,0 +1,68 @@
+<#
+Copyright 2016 ASOS.com Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.NAME
+    Get-ScriptBodyFromScriptText
+
+.SYNOPSIS
+    Returns the powershell script with the metadata variables removed
+#>
+function Get-ScriptBodyFromScriptText
+{
+
+    param
+    (
+
+        [Parameter(Mandatory=$true)]
+        [string] $Script,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("ScriptModule", "StepTemplate")]
+        [string] $Type
+
+    )
+
+    $ErrorActionPreference = "Stop";
+    Set-StrictMode -Version "Latest";
+
+    switch( $Type )
+    {
+
+        "ScriptModule" {
+            $variablesToRemove = @( "ScriptModuleName", "ScriptModuleDescription" );
+        }
+
+        "StepTemplate" {
+            $variablesToRemove = @( "StepTemplateName", "StepTemplateDescription", "StepTemplateParameters" );
+        }
+
+    }
+
+    $newScript = $Script;
+    foreach( $variableName in $variablesToRemove )
+    {
+        $variableScript = Get-VariableStatementFromScriptText -Script $newScript -VariableName $variableName -Type "Statement";
+        if( -not [string]::IsNullOrEmpty($variableScript) )
+        {
+            $newScript = $newScript.Replace($variableScript, "");
+        }
+    }
+    $newScript = $newScript.Trim();
+
+    return $newScript;
+x
+}
---
+++ OctopusStepTemplateCi/Cmdlets/Internal/PowerShellManipulation/Get-VariableStatementFromScriptText.ps1
@@ -0,0 +1,72 @@
+<#
+Copyright 2016 ASOS.com Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.NAME
+    Get-VariableStatementFromScriptText
+
+.SYNOPSIS
+    Returns the variable assignment statement for a given variable name that is in a powershell script
+#>
+function Get-VariableStatementFromScriptText
+{
+
+    param
+    (
+
+        [Parameter(Mandatory=$true)]
+        [string] $Script,
+
+        [Parameter(Mandatory=$true)]
+        [string] $VariableName,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("Statement", "Value")]
+        [string] $Type
+
+    )
+
+    $tokens = $null;
+    $parseErrors = $null;
+    $ast = [System.Management.Automation.Language.Parser]::ParseInput($Script, [ref] $tokens, [ref] $parseErrors);
+
+    $filter = {
+        ($args[0] -is [System.Management.Automation.Language.AssignmentStatementAst]) -and
+        ($args[0].Left -is [System.Management.Automation.Language.VariableExpressionAst]) -and
+        ($args[0].Left.VariablePath.UserPath -eq $VariableName)
+    }
+
+    $variableStatement = $ast.Find($filter, $true);
+    
+    if( $variableStatement -eq $null )
+    {
+        return $null;
+    }
+
+    switch( $Type )
+    {
+
+        "Statement" {
+            return $variableStatement | % Extent | % Text;
+        }
+
+        "Value" {
+            return $variableStatement | % Right | % Extent | % Text;
+        }
+
+    }
+
+}
