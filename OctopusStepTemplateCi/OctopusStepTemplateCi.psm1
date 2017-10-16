434d4326d29c969841a243aa9d4728041f78d34f
Michael Clayton <Michael.Clayton@asos.com>
2017-10-16 12:41:36 +0100

    Removed old Write-TeamCityMessage implementation

--- OctopusStepTemplateCi/OctopusStepTemplateCi.psm1
+++ OctopusStepTemplateCi/OctopusStepTemplateCi.psm1
@@ -59,7 +59,6 @@ Set-StrictMode -Version Latest
 . "$(Join-Path $PSScriptRoot '\Cmdlets\Internal\TeamCity\ServiceMessages\Write-TeamCityProgressMessage.ps1')"
 . "$(Join-Path $PSScriptRoot '\Cmdlets\Internal\TeamCity\ServiceMessages\Write-TeamCityServiceMessage.ps1')"
 . "$(Join-Path $PSScriptRoot '\Cmdlets\Internal\TeamCity\Reset-BuildOutputDirectory.ps1')"
-. "$(Join-Path $PSScriptRoot '\Cmdlets\Internal\TeamCity\Write-TeamCityMessage.ps1')"
 . "$(Join-Path $PSScriptRoot '\Cmdlets\Internal\Tests\Get-ScriptValidationTestsPath.ps1')"
 . "$(Join-Path $PSScriptRoot '\Cmdlets\Internal\Tests\Invoke-PesterForTeamCity.ps1')"
 . "$(Join-Path $PSScriptRoot '\Cmdlets\Internal\Tests\Update-XPathValue.ps1')"
