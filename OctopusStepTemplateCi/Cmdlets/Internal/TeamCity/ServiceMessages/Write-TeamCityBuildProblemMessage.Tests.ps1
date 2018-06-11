7e8ca5b95ba0f7939d3e75c5eddab712b32e5d1d
Michael Clayton <Michael.Clayton@asos.com>
2018-06-11 15:43:32 +0100

    Added Write-TeamCityBuildProblemMessage.ps1

---
+++ OctopusStepTemplateCi/Cmdlets/Internal/TeamCity/ServiceMessages/Write-TeamCityBuildProblemMessage.Tests.ps1
@@ -0,0 +1,48 @@
+<#
+Copyright 2016 ASOS.com Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.NAME
+    Write-TeamCityBuildProblemMessage.Tests
+
+.SYNOPSIS
+    Pester tests for Write-TeamCityBuildProblemMessage.
+#>
+
+$ErrorActionPreference = "Stop";
+Set-StrictMode -Version "Latest";
+
+InModuleScope "OctopusStepTemplateCi" {
+
+    Describe "Write-TeamCityBuildProblemMessage" {
+
+        Mock -CommandName "Write-Host" `
+             -MockWith {
+                 throw "write-host should not be called with (`$Object='$Object')";
+             };
+
+        It "Should write the message to the powershell host" {
+            Mock -CommandName "Write-Host" `
+                 -ParameterFilter { $Object -eq "##teamcity[buildProblem description='my description' identity='my identity']" } `
+                 -MockWith {} `
+                 -Verifiable;
+            Write-TeamCityBuildProblemMessage -Description "my description" -Identity "my identity";
+            Assert-VerifiableMock;
+        }
+
+    }
+
+}
---
+++ OctopusStepTemplateCi/Cmdlets/Internal/TeamCity/ServiceMessages/Write-TeamCityBuildProblemMessage.ps1
@@ -0,0 +1,49 @@
+<#
+Copyright 2016 ASOS.com Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.NAME
+    Write-TeamCityBuildProblemMessage
+
+.SYNOPSIS
+    Outputs a 'buildProblem' service message for TeamCity
+#>
+function Write-TeamCityBuildProblemMessage
+{
+
+    param
+    (
+
+        [Parameter(Mandatory=$true)]
+        [string] $Description,
+
+        [Parameter(Mandatory=$false)]
+        [string] $Identity
+
+    )
+
+    $values = @{
+        "description" = $Description
+    };
+
+    if( -not [string]::IsNullOrEmpty($Identity) )
+    {
+        $values.Add("identity", $Identity);
+    }
+    
+    Write-TeamCityServiceMessage -MessageName "buildProblem" -Values $values;
+
+}
